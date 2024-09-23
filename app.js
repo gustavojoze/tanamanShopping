eaf82fc65ef3d2a09770095edb7729b38088fb9d
gustavojoze <gustavojosesoaressantana@gmail.com>
2024-09-22 21:06:59 -0300

    Página principal feita com as funcionalidades de abrir e fechar o modal, junto com abrir e fechar o carrinho.

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-    
+    const switchModal = () => {
+        const modal = document.querySelector('.contentModal') 
+        const actualStyle = modal.style.display 
+        if (actualStyle == 'block')
+             {
+                 modal.style.display = 'none'
+             } 
+        else {
+             modal.style.display = 'block'
+             } 
+    }
+        
+    const switchCar = () => {
+        const containerCar = document.querySelector('.active_container_carrinho') 
+        const actualStyle = containerCar.style.display 
+        if (actualStyle == 'block')
+             {
+                containerCar.style.display = 'none'
+             } 
+        else {
+            containerCar.style.display = 'block'
+             } 
+    }
+        
+    const countBuy = () =>{
+        const iconCount = document.querySelector('.header_icons_cicleCount_p')
+        iconCount.innerHTML = 2
+            
+    }
