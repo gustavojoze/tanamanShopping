29322abb2194cb581bb9aa73b94bcc9bcb69a694
Gustavo Balbino <guto9201@gmail.com>
2024-10-03 21:01:22 -0300

    Modificacoes pagina de cadastro

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+function mostrarSenha(){
+    var inputPass = document.getElementById('senha');
+    var btnShowPass = document.getElementById('btn-senha');
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type', 'text');
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
+    }else{
+        inputPass.setAttribute('type', 'password');
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
+    }
+}
+function mostrarSenhaConf(){
+    var inputPass = document.getElementById('confirmaSenha');
+    var btnShowPass = document.getElementById('btn-conf-senha');
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type', 'text');
+        btnShowPass.classList.replace('bi-eye-fill', 'bi-eye-slash-fill');
+    }else{
+        inputPass.setAttribute('type', 'password');
+        btnShowPass.classList.replace('bi-eye-slash-fill', 'bi-eye-fill');
+    }
+}
+
+
 document.querySelectorAll('.nav_li').forEach(item => {
     item.addEventListener('click', function() {
         // Remove a classe 'active' de todos os itens
